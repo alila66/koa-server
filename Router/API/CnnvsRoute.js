6f55a7e0410fbbcd966a755586591346b9c430fe
alila <908609588@qq.com>
2019-09-20 13:12:22 +0800

    add show cnnvs all function

--- Router/API/CnnvsRoute.js
+++ Router/API/CnnvsRoute.js
@@ -6,12 +6,24 @@ const Cnnv = require('../../Models/CnnvModel')
 
 /**
  * @route Post /api/cnnvs/show
- * @description 测试接口
+ * @description 返回一个记录
  * @access  接口是公开的
  */
 router.get("/show", async ctx => {
     await dbOpen()
     let findResult = await Cnnv.findOne()
+    ctx.body = findResult
+    await dbClose()
+})
+
+/**
+ * @route Post /api/cnnvs/showall
+ * @description 返回多个记录
+ * @access  接口是公开的
+ */
+router.get("/showall", async ctx => {
+    await dbOpen()
+    let findResult = await Cnnv.find().limit(20)
     ctx.body = findResult
     await dbClose()
 })
